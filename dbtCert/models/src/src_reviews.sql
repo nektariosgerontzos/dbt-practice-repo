b360c3198e69ba3c98aa1fb9eb172fa51f8af8ea
Nektarios Gerontzos <nektariosgerontzos@gmail.com>
2026-05-31 11:01:11 +0000

    better one

--- dbtCert/models/src/src_reviews.sql
+++ dbtCert/models/src/src_reviews.sql
@@ -1,7 +1,12 @@
 WITH dataset AS (
     SELECT * FROM AIRBNB.RAW.RAW_REVIEWS
 )
-SELECT * 
+SELECT 
+    listing_id,
+    date AS review_date,
+    reviewer_name,
+    comments AS review_texts,
+    sentiment AS review_sentiment
 FROM dataset
 
 
